0062cb2bccc3b46211a46c89d0dd6b1b3945cc39
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-07-20 13:25:58 +0300

    fix: add data

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -2,7 +2,7 @@ import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, Ion
 import axios from "axios";
 import { addCircle, arrowBackCircle, arrowForwardCircle } from 'ionicons/icons';
 import { useEffect, useRef, useState } from 'react';
-import { posts1, posts2, posts3, posts4, posts5, posts6, posts7 } from '../data/data';
+import { posts } from '../data/data';
 import './ExploreContainer.css';
 
 
@@ -15,8 +15,7 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
   
   const [desc, setDesc] = useState<boolean>(false);
   const [descbutton, setDescbutton] = useState<string>("More details");
-  const [test, setTest] = useState<{title:string, company:string, contract:string, description:string, limit_date:string, }[]>([]);
-  const [test2, setTest2] = useState<{title:string, company:string, contract:string, description:string, limit_date:string, }[]>([]);
+  const [test, setTest] = useState<{title:string, company:string, contrat:string, description:string, date:string, }[]>(posts);
   const [id, setId] = useState<number>(1);
   const ref = useRef<any>(null);
   const [ alertError ] = useIonAlert();
@@ -26,7 +25,6 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
       console.log(response);
       
       setTest(response.data.information);
-      setTest2(response.data.information);
     })
     .catch((err)=>{
       alertError({
@@ -69,13 +67,11 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
   }
 
   function filterData(e:any) {
-    console.log("Hello World");
-    
     if(e.target.value === ""){
-      setTest(test2)
+      setTest(posts)
     }
     else{
-      const data = test.filter(elt => elt.title.includes(e.target.value));
+      const data = posts.filter(elt => elt.title.includes(e.target.value));
       setTest(data);
     }
   }
@@ -85,18 +81,18 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
       <div className="container">
         <IonSearchbar onIonChange={filterData} ref={ref}></IonSearchbar>
         {
-          test.map((item:{title:string, company:string, contract:string, description:string, limit_date:string, }, index:number) => (
+          test.map((item:{title:string, company:string, contrat:string, description:string, date:string, }, index:number) => (
             <IonCard key = {index} className='content'>
               <IonCardHeader>
                 <IonCardTitle className='title'>{item.title.toLocaleUpperCase()}</IonCardTitle>
               </IonCardHeader>
               <IonCardContent>
               <IonCardSubtitle className='company'>{item.company}</IonCardSubtitle>
-                <IonCardSubtitle>{item.contract}</IonCardSubtitle>
+                <IonCardSubtitle>{item.contrat}</IonCardSubtitle>
                 {
                   desc && <IonCardSubtitle>{item.description}</IonCardSubtitle>
                 }
-                <IonCardSubtitle>{item.limit_date}</IonCardSubtitle>
+                <IonCardSubtitle>{item.date}</IonCardSubtitle>
                 <button className='details' onClick={showDesc}><IonIcon slot='start' md={addCircle} /> {descbutton}</button>
               </IonCardContent>
             </IonCard>
